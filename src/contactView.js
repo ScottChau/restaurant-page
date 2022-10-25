64403868a85ccd98c0e7d5cc9fac7fab8a6ecdd5
Scott Chau <scottshing@yahoo.com.hk>
2022-10-25 18:50:39 -0400

    cool

--- src/contactView.js
+++ src/contactView.js
@@ -1,13 +1,23 @@
-export default function contactView() {
-  const main = document.querySelector(".main");
-  main.innerHTML = "";
+function createContact() {
+  const contact = document.createElement("div");
   const email = document.createElement("h5");
   const phone = document.createElement("h5");
   const address = document.createElement("h5");
+
+  contact.classList.add("contact");
 
   email.textContent = "Email: scottshing@yahoo.com.hk";
   phone.textContent = "Phone: 999-999-9999";
   address.textContent = "Address: Toronto";
 
-  main.append(email, phone, address);
+  contact.append(email, phone, address);
+
+  return contact;
+}
+
+export default function contactView() {
+  const main = document.querySelector(".main");
+  main.innerHTML = "";
+
+  main.appendChild(createContact());
 }
--- src/homeView.js
+++ src/homeView.js
@@ -1,12 +1,21 @@
-export default function homeView() {
-  const main = document.querySelector(".main");
+function createHomePage() {
+  const homePage = document.createElement("div");
   const headline = document.createElement("h1");
   const image = document.createElement("img");
 
-  main.innerHTML = "";
+  homePage.classList.add("homePage");
+
   image.src =
     "http://secure.s.forbestravelguide.com/img/properties/one-restaurant/one-restaurant.jpg";
   headline.textContent = "The Number One Restaurant in Toronto";
 
-  main.append(headline, image);
+  homePage.append(headline, image);
+
+  return homePage;
+}
+
+export default function homeView() {
+  const main = document.querySelector(".main");
+  main.innerHTML = "";
+  main.appendChild(createHomePage());
 }
